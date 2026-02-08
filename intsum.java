050766214543e1a466b9a310793dc229f7a4fdd3
MeghasuryaB <meghasuryabaskaran@gmail.com>
2026-02-08 11:30:15 +0530

    sum of values in integer

--- intsum.java
+++ intsum.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 
 public class Intsum {
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int n=s.nextInt();
-        int total=0;
-        while (n!=0){
-            int digit=n%10;
-            total=total+digit;
-            n=n/10;
+        try (Scanner s = new Scanner(System.in)) {
+            int n=s.nextInt();
+            int total=0;
+            while (n!=0){
+                int digit=n%10;
+                total=total+digit;
+                n=n/10;
+            }
+            System.out.println("Total="+total);
         }
-        System.out.println("Total="+total);
-        s.close();
     }
 }
