4448320df40079e2af080d0a3a6c5acfa7ab5c0e
MeghasuryaB <meghasuryabaskaran@gmail.com>
2026-02-09 12:58:55 +0530

    optimised prime factor

--- primefactors.java
+++ primefactors.java
@@ -3,12 +3,15 @@ public class primefactors {
     static void primefactors1(int n){
         int i=2;
         
-        while(n>1){
+        while(i*i<=n){
         while(n%i==0){
             System.out.println(i);
             n=n/i;
         }
         i++;
+    }
+    if(n>1){
+        System.out.println(n);
     }
 
 
